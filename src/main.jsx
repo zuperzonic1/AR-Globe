4ac6e5f765fcc800bcce17d95fcd1fd600d9019c
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-06-08 12:53:24 -0400

    refactor: Update Earth3D component to use larger sphere size and remove unnecessary code

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import  { useRef, useEffect } from 'react';
 import { useLoader, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
@@ -19,8 +19,8 @@ const Earth3D = () => {
 
     return (
         <mesh ref={meshRef} position={[0, 0, 0]}>
-            <sphereGeometry args={[1, 32, 32]} />
-            <meshStandardMaterial map={colorMap} normalScale={[0.5, 0.5]} />
+            <sphereGeometry args={[2.5, 32, 32]} />
+            <meshStandardMaterial map={colorMap} />
         </mesh>
     );
 };
